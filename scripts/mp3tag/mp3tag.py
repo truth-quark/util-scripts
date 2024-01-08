a2882bd50e0d5d3e8a81d830ede4bbaa32076587
Ben Davies <bens.public@gmail.com>
2024-01-08 12:46:14 +1100

    Implement cmd line processing & tagging separate files.

--- scripts/mp3tag/mp3tag.py
+++ scripts/mp3tag/mp3tag.py
@@ -5,6 +5,7 @@ import glob
 import pathlib
 import subprocess
 import warnings
+import argparse
 
 # TODO: add option for title only/title+artist when no track # exists
 
@@ -25,7 +26,8 @@ pattern = re.compile(NUMBERED_ALBUM_PATTERN)
 TRACK_NUMBERING_PATTERN = r"^[0-9]{1,3}"
 track_pattern = re.compile(TRACK_NUMBERING_PATTERN)
 
-
+ARTIST_NAME = "artist-name"
+FILES_OR_DIRS = "files-or-dirs"
 DEBUG = "DEBUG" in os.environ
 
 
@@ -94,14 +96,41 @@ def tag_file(path: pathlib.Path, track: int, title: str, album: str, artist: str
         print(err)
 
 
+def tag_title_only(path: pathlib.Path, title: str, album: str, artist: str):
+    # TODO: check/strip leading numbering from files for title
+    cmd = f"""id3tool -t "{title}" -r '{artist}' "{path}" """
+    res = subprocess.run(cmd, shell=True)
+
+    if DEBUG:
+        print("DEBUG:", cmd)
+
+    if res.returncode:
+        err = f"Failed to exec: {cmd}"
+        print(err)
+
+
 class TagError(Exception):
     pass
 
 
 if __name__ == "__main__":
-    usage = "mp3tag [artist name] [album_dir(s)]"
-    _artist = sys.argv[1]
-    _dirs = sys.argv[2:]
+    # usage = "mp3tag [artist name] [file(s) or album_dir(s)]"
+    parser = argparse.ArgumentParser(description="Update tags for batches of MP3 files")
+    parser.add_argument(ARTIST_NAME, help="Artist/band name")
+    parser.add_argument(FILES_OR_DIRS,
+                        type=str,
+                        nargs="+",
+                        metavar="F",
+                        help="Files or directories of files to tag")
 
-    for d in _dirs:
-        tag_files(pathlib.Path(d), _artist)
+    raw_args = parser.parse_args()
+    args = vars(raw_args)
+
+    for d in args[FILES_OR_DIRS]:
+        if os.path.isdir(d):
+            tag_files(pathlib.Path(d), args[ARTIST_NAME])
+        else:
+            tag_title_only(pathlib.Path(d),
+                           os.path.basename(d),
+                           "-",  # no album
+                           args[ARTIST_NAME])
