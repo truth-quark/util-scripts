b0cb5f51bfd3b8e21fdd9aaf02af23d8b53ac62f
Ben Davies <bens.public@gmail.com>
2024-12-27 21:58:35 +1100

    Refactor: rename function for clarity.

--- scripts/emptydirs.py
+++ scripts/emptydirs.py
@@ -5,7 +5,7 @@ import sys
 args = sys.argv[1:] if len(sys.argv) > 1 else [os.getcwd()]
 
 
-def is_included(dirpath, dirnames, filenames):
+def is_empty(dirpath, dirnames, filenames):
     if dirpath.endswith(("/.git", "/.hg")):
         return False
 
@@ -14,5 +14,5 @@ def is_included(dirpath, dirnames, filenames):
 
 for root in args:
     for dirpath, dirnames, filenames in os.walk(root):
-        if is_included(dirpath, dirnames, filenames):
+        if is_empty(dirpath, dirnames, filenames):
             print(f"{dirpath}")
