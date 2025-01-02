e9c68dd6954949793045ad2eb2b54a17b45abf8a
Ben Davies <bens.public@gmail.com>
2025-01-02 13:27:29 +1100

    Refactor logic for handling multiple media path patterns.

--- scripts/abc_listen_rip.py
+++ scripts/abc_listen_rip.py
@@ -4,7 +4,13 @@ import sys
 import requests
 
 urls = sys.argv[1:]
-pattern = r"https://mediacore-live-production.akamaized.net/audio/../../Z/..[.]mp3"
+
+# Media file patterns can be:
+# https://abcmedia.akamaized.net/something/some_podcast/name-2021-10-4-descriptor.mp3
+# https://abcmedia.akamaized.net/rn/podcast/2022/02/eot_20220207.mp3
+patterns = [r"https://mediacore-live-production.akamaized.net/audio/../../Z/..[.]mp3",
+            r"http[s]{0,1}://abcmedia.akamaized.net/[\S]+[.]mp3"]
+
 title_pattern = r"<title>(?P<title>.+)</title>"
 
 for url in urls:
@@ -23,10 +29,12 @@ for url in urls:
         print(f"No title found in HTML, skipping {url}")
         continue
 
-    if match := re.search(pattern, r.text):
-        m_url = match.group()
-        cmd = f"""wget {m_url} -O "{title}.mp3" """
-        os.system(cmd)  # TODO: replace with subprocess
-    else:
-        print(f"No media URL found in {url}")
-        continue
+    # TODO: refactor to find a media path or handle no match
+    for pattern in patterns:
+        if match := re.search(pattern, r.text):
+            m_url = match.group()
+            cmd = f"""wget {m_url} -O "{title}.mp3" """
+            os.system(cmd)  # TODO: replace with subprocess
+        else:
+            print(f"No media URL found in {url}")
+            continue
