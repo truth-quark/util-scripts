8863e1f49a7e038e238482bc4df1ad0232e75621
Ben Davies <bens.public@gmail.com>
2025-01-12 12:51:25 +1100

    Refactor timestamp str creation.

--- scripts/rss2sh.py
+++ scripts/rss2sh.py
@@ -8,7 +8,7 @@ import feedparser
 
 # TODO: refactor, only extract data with this func
 def entry(e):
-    d = datetime.datetime(*e.published_parsed[:7])
+    timestamp = get_timestamp(e)
 
     url = None
     try:
@@ -19,8 +19,20 @@ def entry(e):
     if url is None:
         url = e.media_content[0]["url"].partition("?")[0]
 
-    # return f"wget {url} -O '{d.year}{d.month}{d.day}-{e.title}.mp3'"
-    return f"wget {url} -O '{d.year}{str(d.month).zfill(2)}{str(d.day).zfill(2)}-{e.title}.mp3'"
+    return f"wget {url} -O '{timestamp}-{e.title}.mp3'"
+
+
+def get_timestamp(entry):
+    d = datetime.datetime(*entry.published_parsed[:7])
+    year = d.year
+
+    # add leading zeros as numerical f string formatting doesn't allow this
+    month = str(d.month).zfill(2)
+    day = str(d.day).zfill(2)
+    hour = str(d.hour).zfill(2)
+    minute = str(d.minute).zfill(2)
+
+    return f"{year}{month}{day}-{hour}{minute}"
 
 
 if __name__ == "__main__":
